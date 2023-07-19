2d5d23c9bf60d5668768b0e443c6853148187ae7
Bebo7897 <81861464+Bebo7897@users.noreply.github.com>
2023-07-19 13:17:28 -0400

    Wrote some basic JavaScript

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,8 @@
+var string = "\"This is my string!\" said: " //sets value of string
+var author = "Brendan O\'Hara" //sets value of author
+var myString = string+author //sets value of myString by concatenating string and author
+
+var Family="The Boothes", Son1 = "Trey" , Son2 = "Ethan" //sets value of Family, Son1, and Son2 variables
+
+window.alert(3+3); //displays 6 in pop up window
+document.write(Son1); //writes value to screen
